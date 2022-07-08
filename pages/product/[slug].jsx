24abbc117b5f9a1e6b64302fb1ab8d160e83bf5c
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2022-07-08 15:42:54 +0800

    single product page

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -13,12 +13,16 @@ function ProductScreen() {
 
     return (
     <Layout title={ product.name }>
+
+        {/* RETURN to HOME */}
         <div className="py-2">
             <Link href="/">back to products</Link>
         </div>
 
-        <div className="grid md:grid-cols-4 md:grid-gap-3">
-            {/* 2 columns of 4 columns */}
+        {/* PRODUCT INFO - 3 columns */}
+        <div className="grid md:grid-cols-4 md:gap-3">
+            {/* Note: 2 columns of 4 columns - col-span-2 */}
+            {/* a. image (col - 1)*/}
             <div className="md:col-span-2">
                 <Image
                     src={ product.image }
@@ -27,7 +31,35 @@ function ProductScreen() {
                     height={ 640 }
                     layout="responsive"
                 />
-                
+            </div>
+            {/* b. product desc (col -2) */}
+            <div>
+                <ul>
+                    <li>
+                        <h1 className="text-lg">{ product.name }</h1>
+                    </li>
+                    <li>Category: { product.category }</li>
+                    <li>Brand: { product.brand }</li>
+                    <li>{ product.rating } of { product.numReviews } reviews</li>
+                    <li>Description: { product.description }</li>
+                </ul>
+            </div>
+            {/* c. product price (col - 3) */}
+            <div>
+                <div className="card p-5">
+                    {/* i. price */}
+                    <div className="mb-2 flex justify-between">
+                        <div>Price</div>
+                        <div>${ product.price }</div>
+                    </div>
+                    {/* ii. status */}
+                    <div className="mb-2 flex justify-between">
+                        <div>Status</div>
+                        <div>{ product.countInStock > 0 ? 'In stock' : 'Unavailable' }</div>
+                    </div>
+                    {/* iii. add to cart */}
+                    <button className="primary-button w-full">Add to cart</button>
+                </div>
             </div>
         </div>
     </Layout>
